32c17b9855700ccaeda2eea0c100965a43a53132
ianleckie <ianleckie@hey.com>
2021-09-03 00:00:24 -0500

    Cleaned up copy and JS.

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
-function checkVisibility( element ) {
+function checkVisibility( element_id ) {
 
 	var viewport_height = $(window).height();
 	var scroll_top      = $(window).scrollTop();
-	var element_top     = $(element).offset().top;
-	var element_height  = $(element).outerHeight( true );
+	var element_top     = $(element_id).offset().top;
+	var element_height  = $(element_id).outerHeight( true );
 
 	return ( ( element_top < ( viewport_height + scroll_top ) ) && ( element_top > ( scroll_top - element_height ) ) );
 
@@ -11,25 +11,28 @@ function checkVisibility( element ) {
 
 function updateSectionNumber() {
 
-	var section_ids = ['#intro', '#experience', '#more-about-me', '#resume', '#colophon'];
-	var section_div = $('#section-num');
+	var section_ids     = ['#intro', '#experience', '#more-about-me', '#resume', '#colophon'];
+	var section_element = $('#section-num');
 
-	for (var key = 0; key < section_ids.length - 1; key++) {
+	for (var key = 1; key < section_ids.length - 1; key++) {
 
 		if ( checkVisibility( section_ids[0] ) ) {
-			section_div.html( 1 );
+			
+			section_element.html( 1 );
 			break;
-		} else if ( key > 0 && !checkVisibility( section_ids[key - 1] ) && checkVisibility( section_ids[key] ) ) {
-			section_div.html( key + 1 );
+		
+		} else if ( !checkVisibility( section_ids[key - 1] ) && checkVisibility( section_ids[key] ) ) {
+			
+			section_element.html( key + 1 );
 			break;
+		
 		}
+	
 	}
 
 }
 
 function loadFunction( jQuery ) {
-
-	updateSectionNumber();
 
 	$( window ).scroll( updateSectionNumber );
 
