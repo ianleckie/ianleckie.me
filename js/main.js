731283c75208a0f4fa3cab095ce01e372e454624
ianleckie <ianleckie@hey.com>
2021-09-03 17:32:45 -0500

    Implemented overlay and gallery functionality.

--- js/main.js
+++ js/main.js
@@ -1,10 +1,14 @@
+var article_number_selector   = '#article-number',
+    overlay_selector          = '#overlay',
+    overlay_contents_selector = '#overlay div';
+
 /*
 Checks whether an element is in the viewport
 */
 function checkVisibility( element ) {
 
-	var viewport_height = $(window).height();
-	var scroll_top      = $(window).scrollTop();
+	var viewport_height = $( window ).height();
+	var scroll_top      = $( window ).scrollTop();
 	var element_top     = element.offset().top;
 	var element_height  = element.outerHeight( true );
 
@@ -20,12 +24,12 @@ updated with that value
 */
 function updateArticleNumber() {
 	
-	var article_number  = $('#article-number');
+	var article_number  = $( article_number_selector );
 	var last_is_visible = false;
 
-	$('article').each(function( index ) {
+	$( 'article' ).each( function( index ) {
 
-		is_visible = checkVisibility( $(this) );
+		is_visible = checkVisibility( $( this ) );
 
 		if ( !last_is_visible && is_visible ) {
 			
@@ -40,9 +44,72 @@ function updateArticleNumber() {
 
 }
 
+/*
+Loads a link's href into the gallery rather than following the link
+*/
+function updateGallery( event ) {
+
+	event.preventDefault();
+
+	$( 'a' ).removeClass( 'selected ');
+	$( this ).addClass( 'selected' );
+
+	$( '#gallery' ).load( $(this).attr( 'href' ) );
+
+}
+
+/*
+Loads a link's href into the overlay rather than following the link
+*/
+function openLinkInOverlay( event ) {
+	
+	event.preventDefault();
+
+	toggleOverlay();
+
+	$( overlay_contents_selector ).load( $(this).attr( 'href' ) );
+
+}
+
+/*
+Empties overlay, toggles body scrolling
+and then fades overlay in or out
+*/
+function toggleOverlay() {
+	
+	$( overlay_contents_selector ).html( '' );
+
+	$( 'body' ).toggleClass( 'noscroll' );
+
+	$( overlay_selector ).fadeToggle( 300 );
+
+}
+
+/*
+Checks if ESC is pressed while the overlay
+is open and closes it if so
+*/
+function closeOverlayOnEscPress( event ) {
+
+	var code = event.keyCode || event.which;
+
+	if( code == 27 && $( overlay_selector ).is( ':visible' ) ) toggleOverlay();
+
+}
+
 function loadFunction( jQuery ) {
 
 	$( window ).scroll( updateArticleNumber );
+
+	$( window ).keypress( closeOverlayOnEscPress );
+
+	$( '#close' ).click( toggleOverlay );
+
+	$( 'a[target="overlay"]' ).click( openLinkInOverlay );
+	
+	$( 'a[target="gallery"]' ).click( updateGallery );
+
+	$( 'a[target="gallery"]' ).first().click();
 
 }
  
