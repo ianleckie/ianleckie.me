3f133fba1535b19ea1eccb518f7b587778cf51f2
ianleckie <ianleckie@hey.com>
2021-09-16 16:03:35 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -70,13 +70,13 @@ function updateGallery( event ) {
 	$( gallery_link_selector ).removeClass( 'selected ');
 	$( this ).addClass( 'selected' );
 
-	$( gallery_selector ).hide();
+	$( gallery_selector + ' div' ).hide();
 
 	$( gallery_selector ).load( $(this).attr( 'href' ) + ( ( dev_mode ) ? '?t=' + Date.now() : '' ), function () {
 
 		resizeGallery( current_height );
 
-		$( gallery_selector ).show();
+		$( gallery_selector + ' div' ).show();
 
 	} );
 
