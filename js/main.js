d04f0fb3f2b790b5c408cda7f25833208d1fa9fe
ianleckie <ianleckie@hey.com>
2021-09-17 17:28:16 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -109,6 +109,7 @@ function resizeGallery( current_height ) {
 
 	auto_height = $( gallery ).css( 'height', 'auto' ).height();
 
+	// adjust for iframe
 	auto_height = ( $( gallery + ' .video-container' ).length ) ? auto_height + 16 : auto_height;
 
 	$( gallery ).height( current_height ).height( auto_height );
